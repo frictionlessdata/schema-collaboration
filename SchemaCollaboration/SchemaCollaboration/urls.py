660e433a1d42249727c8ec371521cdf04e4d8034
Carles Pina i Estany <carles@pina.cat>
2020-08-04 12:48:51 +0200

    Add support for static files

--- SchemaCollaboration/SchemaCollaboration/urls.py
+++ SchemaCollaboration/SchemaCollaboration/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
