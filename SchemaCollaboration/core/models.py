43593f7fbf605bd232048ad030616890f56e8486
Carles Pina i Estany <carles@pina.cat>
2020-08-05 16:49:15 +0200

    Add create/modify on columns in the list of schemas

--- SchemaCollaboration/core/models.py
+++ SchemaCollaboration/core/models.py
@@ -3,7 +3,20 @@ import uuid as uuid_lib
 from django.db import models
 
 
-class Schema(models.Model):
+class CreateModifyOn(models.Model):
+    """Details of data creation and modification: including date, time and user."""
+    objects = models.Manager()  # Helps Pycharm CE auto-completion
+
+    created_on = models.DateTimeField(help_text='Date and time at which the entry was created', auto_now_add=True,
+                                      blank=False, null=False)
+    modified_on = models.DateTimeField(help_text='Date and time at which the entry was modified', auto_now=True,
+                                       blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Schema(CreateModifyOn):
     uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False, unique=True)
     schema = models.BinaryField()  # TODO: move it to a file
 
