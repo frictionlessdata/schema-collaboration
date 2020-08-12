0edd7a111b325103a442642c7f6249a7fea515c9
Carles Pina i Estany <carles@pina.cat>
2020-08-12 15:46:43 +0200

    Add unit tests
    Exclude asgi/wsgi files that are not used but might be useful for future
    reference

--- SchemaCollaboration/core/tests/test_views.py
+++ SchemaCollaboration/core/tests/test_views.py
@@ -41,9 +41,36 @@ class ViewsTest(TestCase):
     def test_get(self):
         c = Client()
 
-        schema_text = b'This is a schema test2'
-        schema = Schema.objects.create(schema='This is a schema test2')
+        schema_text = 'This is a schema test2'
+        schema = Schema.objects.create(schema=schema_text)
 
         response = c.get(reverse('api-schema', kwargs={'uuid': schema.uuid}))
 
-        self.assertEqual(response.content, schema_text)
+        self.assertEqual(response.content.decode('utf-8'), schema_text)
+
+    def test_datapackage_no_uuid(self):
+        c = Client()
+
+        response = c.get(reverse('datapackage-ui'))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/static/datapackage-ui/index.html')
+
+    def test_datapackage_uuid(self):
+        c = Client()
+
+        response = c.get(reverse('datapackage-ui') + '?load=this_is_a_potential_uuid')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/static/datapackage-ui/index.html?load=this_is_a_potential_uuid')
+
+    def test_schema_detail(self):
+        c = Client()
+
+        schema_text = b'This is a very special schema'
+        schema = Schema.objects.create(schema=schema_text)
+
+        response = c.get(reverse('schema-detail', kwargs={'uuid': schema.uuid}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, schema_text)
