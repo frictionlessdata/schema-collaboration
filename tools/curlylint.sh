56981e1c7ddc838534748dc699aa27748c6ea98c
Carles Pina i Estany <carles@pina.cat>
2020-09-23 16:37:30 +0100

    Add curlylint support

---
+++ tools/curlylint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# --rule 'indent: "tab"' Disabled, doesn't apply
+
+curlylint --include '\.tmpl' \
+	--rule 'html_has_lang: "en"' \
+	--rule 'aria_role: true' \
+	--rule 'django_forms_rendering: true' \
+	--rule 'image_alt: true' \
+	--rule 'meta_viewport: true' \
+	--rule 'no_autofocus: true' \
+	--rule 'tabindex_no_positive: true' \
+	SchemaCollaboration/core/templates \
+	SchemaCollaboration/management/templates \
+	SchemaCollaboration/comments/templates \
