bd542245ccaf1699095b8b44d2cd1425e85635f7
Carles Pina i Estany <carles@pina.cat>
2020-09-30 15:47:27 +0100

    Improve build-datapackage-ui.sh

--- datapackage-ui-schema-collaboration/build-datapackage-ui.sh
+++ datapackage-ui-schema-collaboration/build-datapackage-ui.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 SCRIPT_BASE_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 
 rsync -arv "$SCRIPT_BASE_DIRECTORY/patch/" "$SCRIPT_BASE_DIRECTORY/../datapackage-ui"
 
+mkdir -p "$SCRIPT_BASE_DIRECTORY/../SchemaCollaboration/core/static/datapackage-ui/dist"
+
+cp "$SCRIPT_BASE_DIRECTORY/patch/index.html" "../SchemaCollaboration/core/static/datapackage-ui"
+
 cd "$SCRIPT_BASE_DIRECTORY/../datapackage-ui"
-
 npm run build
-
-mkdir -p ../SchemaCollaboration/core/static/datapackage-ui/dist
 cp -r dist/* ../SchemaCollaboration/core/static/datapackage-ui/dist/
-cp index.html ../SchemaCollaboration/core/static/datapackage-ui
 
