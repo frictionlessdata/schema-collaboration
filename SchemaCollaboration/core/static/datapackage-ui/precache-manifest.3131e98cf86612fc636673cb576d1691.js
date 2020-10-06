2e830f12b6e9fad1a9014499be0a3a89a456dcba
Carles Pina i Estany <carles@pina.cat>
2020-10-06 15:30:49 +0100

    Updating generated datapackage-ui

---
+++ SchemaCollaboration/core/static/datapackage-ui/precache-manifest.3131e98cf86612fc636673cb576d1691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfd5c7e6e3143112470ac12bbcfd899",
+    "url": "/static/datapackage-ui/index.html"
+  },
+  {
+    "revision": "0f1ff82c537ee37e451c",
+    "url": "/static/datapackage-ui/static/css/2.6d9c54b8.chunk.css"
+  },
+  {
+    "revision": "394794f5ce8c2499af77",
+    "url": "/static/datapackage-ui/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f1ff82c537ee37e451c",
+    "url": "/static/datapackage-ui/static/js/2.3bb0ad45.chunk.js"
+  },
+  {
+    "revision": "2babf1daa2cfc3f8f3b78437b5691cb7",
+    "url": "/static/datapackage-ui/static/js/2.3bb0ad45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394794f5ce8c2499af77",
+    "url": "/static/datapackage-ui/static/js/main.d245f927.chunk.js"
+  },
+  {
+    "revision": "24ec68aed7e427deba7a",
+    "url": "/static/datapackage-ui/static/js/runtime-main.389ccabe.js"
+  }
+]);
--- schema-collaboration-react/build-datapackage-ui.sh
+++ schema-collaboration-react/build-datapackage-ui.sh
@@ -8,11 +8,15 @@ cd "$SCHEMA_COLLABORATION_REACT_DIRECTORY"
 
 if npm run build
 then
-	DESTINATION="../SchemaCollaboration/core/static/datapackage-ui/"
-	mkdir -p "$SCRIPT_BASE_DIRECTORY/../SchemaCollaboration/core/static/datapackage-ui"
+	DESTINATION="$SCHEMA_COLLABORATION_REACT_DIRECTORY/../SchemaCollaboration/core/static/datapackage-ui/"
+	
+	git rm -rf "$DESTINATION"
+	mkdir -p "$DESTINATION"
 
 	rsync -rv --delete build/* ../SchemaCollaboration/core/static/datapackage-ui/
 	echo "Do not edit files in this directory. This is the result of 'npm run build' in $SCHEMA_COLLABORATION_REACT_DIRECTORY. It is in the git repository to facilitate deployment and development of the Django part when it is not needed to change datapackage-ui code" > "$DESTINATION/README.md"
+	git add "$SCHEMA_COLLABORATION_REACT_DIRECTORY/../SchemaCollaboration/core/static/datapackage-ui/"
+
 else
 	echo; echo
 	echo "\"npm run build\" failed"
