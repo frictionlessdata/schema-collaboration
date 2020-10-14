740cac9478d77b03cd84e026f58b9814f4b42f5d
Carles Pina i Estany <carles@pina.cat>
2020-10-14 14:24:30 +0100

    Delete unused code.

--- SchemaCollaboration/comments/views.py
+++ SchemaCollaboration/comments/views.py
@@ -1,10 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.views.generic import TemplateView
 
 from comments.forms import CommentForm
-from core.models import Person, Datapackage
+from core.models import Person
 
 
 def process_post_add_comment(request, context,
@@ -28,33 +27,3 @@ def process_post_add_comment(request, context,
         context['comment_form'] = comment_form
 
         return render(request, failure_template_name, context)
-
-
-class AbstractAddCommentView(TemplateView):
-    def __init__(self, force_anonymous_user, success_view_name, failure_url, *args, **kwargs):
-        self._force_anonymous_user = force_anonymous_user
-        self._success_view_name = success_view_name
-        self._failure_url = failure_url
-
-        super().__init__(*args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        datapackage = Datapackage.objects.get(uuid=kwargs['uuid'])
-
-        if self._force_anonymous_user:
-            person = None
-        else:
-            person = Person.objects.get(user=self.request.user)
-
-        comment_form = CommentForm(request.POST, datapackage_id=datapackage.id, person=person)
-
-        if comment_form.is_valid():
-            comment_form.save()
-            messages.success(request, 'Comment saved')
-            return redirect(reverse(self._success_view_name, kwargs={'uuid': datapackage.uuid}))
-
-        else:
-            messages.error(request, 'Error saving the comment. Check below for the error messages')
-            return redirect()
-            # TODO handle this
-            assert False
