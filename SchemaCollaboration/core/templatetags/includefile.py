075d761e2c31567b2b17beeae86c49266ec9ad53
Carles Pina i Estany <carles@pina.cat>
2020-11-24 16:12:34 +0000

    Fix HTML in the homepage

--- SchemaCollaboration/core/templatetags/includefile.py
+++ SchemaCollaboration/core/templatetags/includefile.py
@@ -10,8 +10,9 @@ register = template.Library()
 
 @register.simple_tag
 def include_homepage():
+    content = ''
     if settings.HOMEPAGE_FILE is None:
-        return mark_safe(f'''
+        content = f'''
         <h1>Welcome to schema-collaboration</h1>
         <p>
             This is the default homepage. Set the environment variable <span style="font-family: monospace;">DEFAULT_HOMEPAGE</span> point at an HTML file to change it.
@@ -23,10 +24,12 @@ def include_homepage():
                     href="{reverse('login')}">management area</a> (login required available on the next page).
             </li>
         </ul>
-    ''')
+    '''
     else:
         file_content = read_file(settings.HOMEPAGE_FILE)
         if file_content is None:
-            return f'''Cannot load {settings.HOMEPAGE_FILE}. Please make sure that it exist and is readable by schema-collaboration.'''
+            content = f'''Cannot load {settings.HOMEPAGE_FILE}. Please make sure that it exist and is readable by schema-collaboration.'''
         else:
-            return file_content
+            content = file_content
+
+    return mark_safe(content)
